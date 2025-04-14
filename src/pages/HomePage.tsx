253fc86ac4df278816b396d00dcbc730ed7d18e5
ajayrajput8 <singhrajputajay2004@gmail.com>
2025-04-14 13:57:07 +0530

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -13,7 +13,8 @@ interface Book {
   location: string;
   createdBy: string,
   imageUrl: string,
-  creator: string
+  creator: string,
+  createdAt: string
 }
 
 
@@ -31,7 +32,8 @@ export const HomePage = () => {
     "location":"",
     "createdBy":"",
     "phone":"",
-    "creator":""
+    "creator":"",
+    "createdAt": new Date().toISOString()
   });
 
   const navigate = useNavigate();
@@ -97,44 +99,12 @@ export const HomePage = () => {
       const addedBook = await response.json();
       setBooks([...books, addedBook]);
       setShowForm(false);
-      setNewBook({"title": "", "author": "", "category": "","createdBy":"","status":"available", "phone": "", "imageUrl": "", "location":"","creator":""});
+      setNewBook({"title": "", "author": "", "category": "","createdBy":"","status":"available", "phone": "", "imageUrl": "", "location":"","creator":"",createdAt: new Date().toISOString()});
     }
   };
 
   return (
     <div className="container mx-auto px-4 py-8">
-      {/*{showForm && (
-        <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
-          <div className="bg-white p-4 sm:p-6 rounded-lg w-full max-w-md">
-            <div className="flex justify-between items-center mb-4">
-              <h2 className="text-xl font-bold">Add New Book</h2>
-              <X className="cursor-pointer" onClick={() => setShowForm(false)} />
-            </div>
-            <form onSubmit={handleSubmit} className="space-y-3">
-              <input type="text" name="title" placeholder="Title" className="w-full border p-2" onChange={handleInputChange} required />
-              <input type="text" name="author" placeholder="Author" className="w-full border p-2" onChange={handleInputChange} required />
-              <input type="text" name="imageUrl" placeholder="Image URL" className="w-full border p-2" onChange={handleInputChange} />
-              <input type="text" name="location" placeholder="Location" className='w-full border p-2' onChange={handleInputChange}/>
-              <select name="category" className="w-full border p-2" onChange={handleInputChange} required>
-                <option value="">Select Category</option>
-                <option value="fiction">Fiction</option>
-                <option value="love">Love</option>
-                <option value="self-dev">Self Develpment</option>
-                <option value="biopic">Biography</option>
-                <option value="inspiring">Inspiring</option>
-                <option value="story">Story</option>
-              </select>
-              <select name="status" className="w-full border p-2" onChange={handleInputChange} required>
-                <option value="">Select Status</option>
-                <option value="available">Available</option>
-                <option value="unavailable">Unavailable</option>
-              </select>
-              <button type="submit" className="w-full bg-blue-600 text-white p-2 rounded">Submit</button>
-            </form>
-          </div>
-        </div>
-      )}*/}
-
         {showForm && (
           <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 px-4">
             <div className="bg-white p-4 sm:p-6 rounded-lg w-full max-w-md">
@@ -210,16 +180,6 @@ export const HomePage = () => {
 
 
       <div className="flex flex-col md:flex-row justify-between items-center mb-8 gap-4">
-        {/*<div className="flex max-w-xl">
-          <Search className="absolute top-32 left-36 text-gray-400 h-5 w-5" />
-          <input
-            type="text"
-            placeholder="Search books by title or author..."
-            className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-        </div>*/}
         <div className="flex max-w-xl w-full px-4 sm:px-0">
           <div className="relative w-full">
             <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-5 w-5" />
@@ -254,6 +214,7 @@ export const HomePage = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
         {filteredBooks
         .filter(book => book.status==='available')
+        .sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
         .map((book,index) => (
           <div key={book.id||index} className="bg-white rounded-lg shadow-md overflow-hidden"> 
             <img
